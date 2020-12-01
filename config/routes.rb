4739e5d596b69482ac3a037db0e87bb1372e3a12
Camille Schull <camille.schull@gmail.com>
2020-12-01 11:50:58 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   resources :shops do
     # so that we have nested URLs, use shallow nesting for certain things
-    resources :products
+    resources :reviews, shallow: true
   end
 
-  resource :cart, only [:show] do
+  resources :products, except [:index]
+
+  resource :cart, only [:show, :new] do
     post '/:product_id', to: 'cart#add_item_to_cart'
     delete '/:product_id', to: 'cart#remove_item_from_cart'
   end
   # method run before_action will create for us
+
+  get '/dashboard', to: 'product#index'
+
 end
