d3321ccde49ce2e9b94cb8ab75a583358df56be7
guillaume bodart <guillaume.bo@hotmail.com>
2020-12-01 14:25:07 +0100

    no changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :shops do
+    resources :product, shallow: true, except: [:dashboard]
+  end
+
+  resources :carts do
+      resources :product_selection
+
+  # make method called dashboard
+  # need a view dashboard.html.erb
+  get "/dashboard", to: "bookings#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
