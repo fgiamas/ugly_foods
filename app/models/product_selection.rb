11ee734905c6c1e567cff8691bb43a0966ebc970
guillaume bodart <guillaume.bo@hotmail.com>
2020-12-03 17:15:58 +0100

    dashboard for users

--- app/models/product_selection.rb
+++ app/models/product_selection.rb
@@ -1,6 +1,7 @@
 class ProductSelection < ApplicationRecord
   belongs_to :product
   belongs_to :cart, touch: true
+  has_one :shop, through: :product
   validate :check_units_or_kg
 
   def check_units_or_kg
@@ -8,4 +9,13 @@ class ProductSelection < ApplicationRecord
       errors.add(:units, "units or kg value must be present")
     end
   end
+
+  def upcoming_selections
+    self.cart.where("pick_up_date < ?", Date.today)
+  end
+
+  def past_selections
+    self.cart.where("pick_up_date >= ?", Date.today)
+  end
+
 end
