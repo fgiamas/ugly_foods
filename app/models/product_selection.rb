5e3a0199d0b0d46e9a65e42d4d085b7b55671223
guillaume bodart <guillaume.bo@hotmail.com>
2020-12-03 18:31:26 +0100

    dashboard display of products and shops per user

--- app/models/product_selection.rb
+++ app/models/product_selection.rb
@@ -10,6 +10,18 @@ class ProductSelection < ApplicationRecord
     end
   end
 
+  def in_the_past?
+    self.cart.pick_up_date <= Date.today
+
+  end
+
+
+
+  def in_the_future?
+    self.cart.pick_up_date > Date.today
+
+  end
+
   def total_price
     self.units ? quantity = self.units : quantity = self.kg
     total_price = quantity * (self.product.price_per_unit * (self.product.discount_percent/100.to_f))
