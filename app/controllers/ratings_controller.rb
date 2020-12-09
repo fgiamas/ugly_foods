a228963cc751271125917b3d65bc1292a2ea1229
Camille Schull <camille.schull@gmail.com>
2020-12-09 11:15:44 +0100

    dash

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,11 +2,14 @@ class RatingsController < ApplicationController
 
   def create
     @rating = Rating.new(rating_params)
+    @rating.shop = Shop.find(params[:shop_id])
     @rating.user = current_user
-    if @rating.save
+    if @rating.save!
       redirect_back(fallback_location: root_path)
+      flash[:notice] = "your review has been submitted"
     else
-      render :new
+      redirect_back(fallback_location: root_path)
+      flash[:alert] = "something went wrong, please try again"
     end
   end
 
