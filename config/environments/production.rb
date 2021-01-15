d26ff2550c0d285165d89279384ecc9be30b6a5d
Piero Micciulla <micciullapiero@gmail.com>
2021-01-15 18:38:40 +0100

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
