06569565afb4801ea8ea3ac49d0d8e0ddabdc78b
Piero Micciulla <micciullapiero@gmail.com>
2021-01-19 14:43:55 +0100

    fixed signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def after_sign_in_path_for(resource)
     dashboard_path
@@ -11,6 +13,12 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :email, :password, :password_confirmation])
   end
 end
 
